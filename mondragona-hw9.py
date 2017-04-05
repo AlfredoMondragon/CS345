f19220f9fcafd6fff10059d83b1fc396021c4399
AlfredoMondragon <amondragon4@live.nmhu.edu>
2017-04-05 15:23:54 -0600

    Add files via upload

---
+++ mondragona-hw9.py
@@ -0,0 +1,20 @@
+import linked_queue
+from linked_queue import LinkedQueue
+
+
+biglist = list(range(1000000))
+
+
+def suma (biglist):
+	sum_of_a_list = 0
+	for i in biglist:
+		sum_of_a_list += i
+	return sum_of_a_list
+
+def sumb():
+	sum_of_a_list2 = sum(biglist)
+
+def pushpop():
+
+
+def queuess():
